3dcdac6939d59c04235cf53760f8dcec56bfcba8
paulo9428 <paulo9428@naver.com>
2019-12-30 15:48:13 +0900

    Initial Commit

---
+++ KartRecordMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KartRecordMobile
+//
+//  Created by 이준수 on 2019/12/30.
+//  Copyright © 2019 이준수. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
