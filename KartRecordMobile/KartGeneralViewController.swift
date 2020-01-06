a6429d69537e47f6d46d93fd0254017246509230
paulo9428 <paulo9428@naver.com>
2020-01-06 14:01:09 +0900

    tab bar controller init

--- KartRecordMobile/KartGeneralViewController.swift
+++ KartRecordMobile/KartGeneralViewController.swift
@@ -11,4 +11,36 @@ import UIKit
 
 class KartGeneralViewController: UIViewController {
     
+    override func viewDidLoad() {
+        self.viewDidLoad()
+        
+        self.view.addSubview(<#T##view: UIView##UIView#>)
+    }
+    override func didReceiveMemoryWarning() {
+        self.didReceiveMemoryWarning()
+    }
+    
+    
+    // Mark: Initialization
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 2)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // Mark: UI
+    let kartImageButton = UIButton().then {
+        
+    }
+    let trackImageButton =
+    
+    let characterImageButton =
+    
+    
 }
+
+// 구현 포인트
+// 카테고리 별 버튼을 클릭했을 때 각 카테고리에 대한 컬렉션 뷰가 
--- KartRecordMobile/MainTabBarController.swift
+++ KartRecordMobile/MainTabBarController.swift
@@ -19,9 +19,27 @@ class MainTabBarController: UITabBarController {
         // Do any additional setup after loading the view.
         
 //        self.view.addSubview(mainImageView)
+        self.navigationItem.prompt = "메인 페이지"
+        
 
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        let firstTab: UIViewController = UserRecordViewController()
+        let secondTab: UIViewController = KartGeneralViewController()
+        
+        let tabs = NSArray(objects: firstTab, secondTab)
+        self.setViewControllers(tabs as? [UIViewController], animated: false)
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(:coder) has not been implemented")
+    }
     
 
 
